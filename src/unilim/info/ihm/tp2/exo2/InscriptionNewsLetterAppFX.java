fd144142dd14147e1dd1080a5f0b6760465488b2
Max Gorce <max.gorce@yahoo.com>
2024-04-11 09:48:02 +0200

    découpage du code javafx correctement et faire les différents liens

--- src/unilim/info/ihm/tp2/exo2/InscriptionNewsLetterAppFX.java
+++ src/unilim/info/ihm/tp2/exo2/InscriptionNewsLetterAppFX.java
@@ -1,5 +1,21 @@
 package unilim.info.ihm.tp2.exo2;
 
-public class InscriptionNewsLetterAppFX {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import unilim.info.ihm.tp2.exo2.view.RootPane;
 
+public class InscriptionNewsLetterAppFX extends Application {
+	public void start(Stage primaryStage) {
+	    RootPane root = new RootPane();
+
+	    Scene scene = new Scene(root, 600, 450);    
+	    primaryStage.setTitle("Application Newsletter");
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
+	  }
+
+	  public static void main(String[] args) {
+	    Application.launch(args);
+	  }
 }
